5312d0f28aeb1348e9a1ab958e1f317cb7cc6f1c
AdhamMostafa74 <adhammada1012@gmail.com>
2026-01-24 16:08:23 +0200

    Changed the redirection to home as the landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,24 @@
-import Image from "next/image";
+'use client'
+import { CarouselComponent } from '@/components/Home Componenets/Carousel'
+import CategoriesCarousel from '@/components/Home Componenets/CategoryCards'
+import TopSellingProductsCard from '@/components/Home Componenets/TopSellingProductsCards'
+
 
 export default function Home() {
+
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h2>
-        hello!
+    <div className='container mt-3'>
+
+      <CarouselComponent />
+
+      <h2 className="text-center text-2xl md:text-3xl lg:text-2xl font-bold text-blue-500 mb-6 border-b-2 py-3">
+        Popular Categories
       </h2>
-    </div>
-  );
+      <CategoriesCarousel />
+      <h2 className="text-center text-2xl md:text-3xl lg:text-2xl font-bold text-blue-500 mb-6 border-b-2 py-3 mt-3">
+        Top Selling Products
+      </h2>
+      <TopSellingProductsCard />
+    </div>)
 }
